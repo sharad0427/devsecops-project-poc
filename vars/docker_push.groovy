bb12d7744d5ec88c68debfcb2ba225cf1cc943fa
Amitabh soni <amitabhdevops2024@gmail.com>
2025-01-27 10:59:13 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,7 +4,7 @@ def call(String projectName, String imageTag, String dockerHubUser) {
     sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
     
     // Tag the Docker image
-    sh "docker image tag ${projectName}:${imageTag} ${dockerHubUser}/${projectName}:${imageTag}"
+    sh "docker image tag ${dockerHubUser}/${projectName}:${imageTag} ${dockerHubUser}/${projectName}:${imageTag}"
     
     // Push the Docker image
     sh "docker push ${dockerHubUser}/${projectName}:${imageTag}"
